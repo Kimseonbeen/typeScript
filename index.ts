6fef20ee0e793ec6126630b0c0736712fcbb6e1f
김선빈 <ksb9242@gmail.com>
2021-07-26 23:12:21 +0900

    typescript arg

---
+++ index.ts
@@ -0,0 +1,12 @@
+const name = "Seonbin",
+    age = 24,
+    gender = "male";
+
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
